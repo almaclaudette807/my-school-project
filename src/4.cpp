4739f90cea8141492dbec33c724b9d9fd019506c
almaclaudette807 <almaclaudette807@gmail.com>
2025-03-09 08:40:02 +0000

    Add 4.cpp

---
+++ src/4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int x = 10;
+    if (x == 10) {
+        cout << "Hello World!" << endl;
+    } else {
+        cout << "Goodbye World!" << endl;
+    }
+    return 0;
+}

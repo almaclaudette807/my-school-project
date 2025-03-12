33b19cf1f3d52ccba21295585f88a1e81c7869c4
almaclaudette807 <almaclaudette807@gmail.com>
2025-03-12 02:49:53 +0000

    Add 7.cpp

---
+++ src/7.cpp
@@ -0,0 +1,9 @@
+int main() {
+    int x = 5;
+    if (x > 3) {
+        cout << "x is greater than 3" << endl;
+    } else {
+        cout << "x is not greater than 3" << endl;
+    }
+    return 0;
+}

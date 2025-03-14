c9bcd170e4c2423df7ee689dcf11c16fac42b4ff
almaclaudette807 <almaclaudette807@gmail.com>
2025-03-14 21:33:53 +0000

    Add 9.cpp

---
+++ src/9.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int x = 5;
+  if (x == 5) {
+    cout << "The value of x is 5." << endl;
+  } else {
+    cout << "The value of x is not 5." << endl;
+  }
+  return 0;
+}

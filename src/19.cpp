3462d98dcacae9fdf51fc90d7a89eb0f5504d366
almaclaudette807 <almaclaudette807@gmail.com>
2025-03-24 07:48:32 +0000

    Add 19.cpp

---
+++ src/19.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1 = 5;
+    int num2 = 3;
+
+    // Add operation
+    int sum = num1 + num2;
+
+    // Subtract operation
+    int difference = num1 - num2;
+
+    // Multiply operation
+    int product = num1 * num2;
+
+    // Divide operation
+    int quotient = num1 / num2;
+
+    // Modulo operation
+    int remainder = num1 % num2;
+
+    // Print results
+    cout << "Sum: " << sum << endl;
+    cout << "Difference: " << difference << endl;
+    cout << "Product: " << product << endl;
+    cout << "Quotient: " << quotient << endl;
+    cout << "Remainder: " << remainder << endl;
+
+    return 0;
+}
